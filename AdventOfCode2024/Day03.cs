60415191165be16ae6ae5eff48ce0d0e67484f62
JLester <ScorpionJL@hotmail.com>
2024-12-22 14:39:35 -0500

    Day 3 Part 2 + refactor

--- AdventOfCode2024/Day03.cs
+++ AdventOfCode2024/Day03.cs
@@ -4,20 +4,29 @@ namespace AdventOfCode2024;
 
 internal static class Day03
 {
-    public static void Solve(bool debug = false)
+    public static void Solve()
     {
         Console.WriteLine("-- Day 3");
 
         string inputFile = @"Day3\input.txt";
         if (!File.Exists(inputFile)) inputFile = @"Day3\test-input.txt";
 
-        var total = File.ReadLines(inputFile)
-            .SelectMany(ValidMulInstructions)
+        var input = File.ReadAllText(inputFile);
+
+        var total = input
+            .SelectValidMulInstructions()
             .Sum(ExecuteInstruction);
-        Console.WriteLine("Total: " + total);
+        Console.WriteLine($"Total: {total}");
+
+        var conditionalTotal = input
+            .SplitOnEnabledInstruction()
+            .Select(RemoveDisabledInstructions)
+            .SelectMany(SelectValidMulInstructions)
+            .Sum(ExecuteInstruction);
+        Console.WriteLine($"Conditional Total: {conditionalTotal}");
     }
 
-    private static string[] ValidMulInstructions(string input) => 
+    private static string[] SelectValidMulInstructions(this string input) =>
         Regex.Matches(input, @"mul\((\d{1,3}),(\d{1,3})\)").Select(m => m.Value).ToArray();
 
     private static int ExecuteInstruction(string instruction)
@@ -28,4 +37,14 @@ internal static class Day03
         var secondNumber = int.Parse(instructionSpan.Slice(comma + 1, instructionSpan.Length - comma - 2));
         return firstNumber * secondNumber;
     }
+    
+    private static string[] SplitOnEnabledInstruction(this string input) => 
+        input.Split("do()", StringSplitOptions.RemoveEmptyEntries);
+
+    private static string RemoveDisabledInstructions(string input) =>
+        input.IndexOf("don't()") switch
+        {
+            -1 => input,
+            var index => input.Substring(0, index)
+        };
 }
