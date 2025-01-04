752189273f30e53f17e340dd46248c08c1004216
JLester <ScorpionJL@hotmail.com>
2025-01-04 11:50:34 -0500

    Part 1 solved

---
+++ AdventOfCode2024/Common.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2024;
+
+internal static class Common
+{
+    public static IEnumerable<(int row, int col)> GridCoordinates(int rows, int cols, int rowStart = 0, int colStart = 0) =>
+        Enumerable.Range(rowStart, rows).SelectMany(row =>
+        Enumerable.Range(colStart, cols), (row, col) => (row, col));
+}
---
+++ AdventOfCode2024/Day06.cs
@@ -0,0 +1,74 @@
+﻿using System.Dynamic;
+using System.Runtime.CompilerServices;
+using Coordinate = (int row, int col);
+
+namespace AdventOfCode2024;
+
+internal static class Day06
+{
+    public static void Solve()
+    {
+        Console.WriteLine("-- Day 6");
+
+        string inputFile = @"Day6\input.txt";
+        if (!File.Exists(inputFile)) inputFile = @"Day6\test-input.txt";
+
+        char[] guardSymbols = ['^', '>', 'v', '<'];
+        var grid = File.ReadLines(inputFile).ToArray();
+        var guard = Common.GridCoordinates(grid.Length, grid[0].Length)
+            .Where(coord => coord.IsGuard(grid))
+            .Select(coord => new Guard(grid[coord.row][coord.col], coord))
+            .First();
+
+        HashSet<Coordinate> visited = [guard.Position, ..grid.Traverse(guard)];
+        Console.WriteLine($"Visited: {visited.Count}");
+    }
+
+    private static IEnumerable<Coordinate> Traverse(this string[] grid, Guard guard)
+    {
+        var rows = grid.Length;
+        var cols = grid[0].Length;
+
+        while (true)
+        {
+            var nextPosition = guard.NextPosition;
+            if (nextPosition.IsExit(rows, cols)) { yield break; }
+            else if (nextPosition.IsObstacle(grid)) { guard.Turn(); }
+            else { yield return guard.MoveTo(nextPosition); }
+        }
+    }
+
+    private static bool IsGuard(this Coordinate position, string[] grid) =>
+        grid[position.row][position.col] == '^' || grid[position.row][position.col] == '>' ||
+        grid[position.row][position.col] == 'v' || grid[position.row][position.col] == '<';
+
+    private static bool IsObstacle(this Coordinate position, string[] grid) => 
+        grid[position.row][position.col] == '#';
+
+    private static bool IsExit(this Coordinate position, int rows, int cols) =>
+        position.row < 0 || position.row >= rows || position.col < 0 || position.col >= cols;
+
+
+    private record struct Guard(char Direction, Coordinate Position)
+    {
+        public Coordinate MoveTo(Coordinate position) => Position = position;
+
+        public readonly Coordinate NextPosition => Direction switch
+        {
+            '^' => (Position.row - 1, Position.col),
+            'v' => (Position.row + 1, Position.col),
+            '<' => (Position.row, Position.col - 1),
+            '>' => (Position.row, Position.col + 1),
+            _ => throw new InvalidOperationException("Invalid guard direction")
+        };
+
+        public void Turn() => Direction = Direction switch
+        {
+            '^' => '>',
+            '>' => 'v',
+            'v' => '<',
+            '<' => '^',
+            _ => throw new InvalidOperationException("Invalid guard direction")
+        };
+    }
+}
