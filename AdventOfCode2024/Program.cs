52d50f44023b9bc6c4a9c3b2b2797f00a649ba54
JLester <ScorpionJL@users.noreply.github.com>
2025-01-19 23:32:44 -0500

    Day 6 solution (#5)

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -2,4 +2,5 @@
 //AdventOfCode2024.Day02.Solve();
 //AdventOfCode2024.Day03.Solve();
 //AdventOfCode2024.Day04.Solve();
-AdventOfCode2024.Day05.Solve();
+//AdventOfCode2024.Day05.Solve();
+AdventOfCode2024.Day06.Solve();
