26b6935dded91c5bdaf3196be08470eefac54916
JLester <ScorpionJL@hotmail.com>
2025-01-26 13:02:57 -0500

    Part 1 solved

---
+++ AdventOfCode2024/Day07.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AdventOfCode2024;
+
+internal static class Day07
+{
+    public static void Solve()
+    {
+        Console.WriteLine("-- Day 7");
+
+        string inputFile = @"Day7\input.txt";
+        if (!File.Exists(inputFile)) inputFile = @"Day7\test-input.txt";
+
+        (long[] values, long expected)[] input = File.ReadLines(inputFile)
+            .Select(line => line.Split(":"))
+            .Select(parts => (
+                parts[1]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(long.Parse)
+                    .ToArray(),
+                long.Parse(parts[0])))
+            .ToArray();
+
+        var calibrationResult = input
+            .Select(GetEquationValue)
+            .Sum();
+        Console.WriteLine($"Calibration result: {calibrationResult}");
+    }
+
+    static long GetEquationValue(this (long[] values, long expected) input) => GetEquationValue(input.values, input.expected);
+
+    static long GetEquationValue(long[] values, long expected) => values
+        .Skip(1)
+        .Aggregate(
+            seed: new HashSet<long> { values[0] },
+            func: (acc, value) => ApplyOperations(acc, value, expected),
+            resultSelector: result => result.Contains(expected) ? expected : 0);
+
+    static HashSet<long> ApplyOperations(IEnumerable<long> state, long value, long max) => state
+        .SelectMany<long, long>(r => [r + value, r * value])
+        .Where(r => r <= max)
+        .ToHashSet();
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -3,4 +3,5 @@
 //AdventOfCode2024.Day03.Solve();
 //AdventOfCode2024.Day04.Solve();
 //AdventOfCode2024.Day05.Solve();
-AdventOfCode2024.Day06.Solve();
+//AdventOfCode2024.Day06.Solve();
+AdventOfCode2024.Day07.Solve();
